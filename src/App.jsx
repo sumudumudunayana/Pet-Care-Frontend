d08f31abe30eaac5a3d5f47256285adbcbd2a144
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-09-13 16:47:34 +0530

    updated

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AppRoutes from "./routes/AppRoutes";
+import { Toaster } from "sonner";
+
+function App() {
+  return (
+    <>
+      <Toaster position="top-right" richColors />
+      <AppRoutes />
+    </>
+  );
+}
+
+export default App;
